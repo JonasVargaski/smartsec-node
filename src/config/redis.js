24b9a11d31ce58361a7721519c327f1969bf3b69
Jonas <jonasvargaski@hotmail.com>
2019-10-11 12:03:53 -0300

    .

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,5 @@
 export default {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  // host: process.env.REDIS_HOST,
+  // port: process.env.REDIS_PORT,
   url: process.env.REDIS_URL,
 };
