f93b2aa46ff201bcd51a5ae4d2a58a076437158b
Jonas <jonasvargaski@hotmail.com>
2019-10-28 12:40:25 -0300

    .

--- src/app/controllers/TestController.js
+++ src/app/controllers/TestController.js
@@ -6,6 +6,28 @@ class TestController {
 
     return res.json(data);
   }
+
+  async store(req, res) {
+    const data = await Device.create({
+      temp: 15,
+      tempAdj: 15,
+      umid: 15,
+      umidAdj: 15,
+      fan: true,
+      alarm: true,
+      workMode: true,
+      lock: true,
+      phase: 15,
+      climate: 15,
+      sensorType: 15,
+      wifiMac: 'teste',
+      wifiPassword: 'teste',
+      firmwareVersion: 'teste',
+      energy: true,
+    });
+
+    return res.json(data);
+  }
 }
 
 export default new TestController();
