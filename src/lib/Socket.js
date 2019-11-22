1518db26cf32a6c305c71e1148ea83e0015a0a9b
Jonas <jonasvargaski@hotmail.com>
2019-11-22 09:17:14 -0300

    add redis config

--- src/lib/Socket.js
+++ src/lib/Socket.js
@@ -1,5 +1,4 @@
 import SocketIO from 'socket.io';
-// import Redis from 'ioredis';
 import MonitoringController from '../app/controllers/MonitoringController';
 
 class Socket {
@@ -18,27 +17,6 @@ class Socket {
 
   middlewares() {
     this.io.set('origins', '*:*');
-
-    // this.io.set(
-    //   'store',
-    //   new SocketIO.RedisStore({
-    //     redisPub: new Redis({
-    //       host: process.env.REDIS_HOST,
-    //       port: process.env.REDIS_PORT,
-    //       keyPrefix: 'socket:',
-    //     }),
-    //     redisSub: new Redis({
-    //       host: process.env.REDIS_HOST,
-    //       port: process.env.REDIS_PORT,
-    //       keyPrefix: 'socket:',
-    //     }),
-    //     redisClient: new Redis({
-    //       host: process.env.REDIS_HOST,
-    //       port: process.env.REDIS_PORT,
-    //       keyPrefix: 'socket:',
-    //     }),
-    //   })
-    // );
 
     // this.io.use((socket, next) => {
     //   const { token } = socket.handshake.query;
